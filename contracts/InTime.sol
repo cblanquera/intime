2a17fad6c978ff2ce402d3f884269cfa36924a5b
Chris Blanquera <chris@incept.asia>
2022-10-07 09:42:12 +0800

    mint and transfer tests

--- contracts/InTime.sol
+++ contracts/InTime.sol
@@ -7,10 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-// ============ Errors ============
-
-error InvalidCall();
-
 // ============ Contract ============
 
 contract InTime is Context, AccessControl, ERC20 {
@@ -18,15 +14,20 @@ contract InTime is Context, AccessControl, ERC20 {
   
   //all custom roles
   bytes32 private constant _MINTER_ROLE = keccak256("MINTER_ROLE");
+  //solidity isnt accurate in math sometimes so we need
+  //to store by the nano to make up for this issue
+  uint256 private constant _TO_MILLISEC = 1000;
 
   // ============ Storage ============
 
-  //recipient count
-  uint256 private _recipients;
+  //the last active accounts count
+  uint256 private _lastActiveAccounts;
+  //the last burned supply 
+  uint256 private _lastBurnedSupply;
 
   // ============ Deploy ============
 
-  constructor(address admin) ERC20("In Time", "MIN") {
+  constructor(address admin) ERC20("In Time", "SEC") {
     //set up roles for the contract creator
     _setupRole(DEFAULT_ADMIN_ROLE, admin);
   }
@@ -38,35 +39,53 @@ contract InTime is Context, AccessControl, ERC20 {
    */
   function balanceOf(
     address account
-  ) public view virtual override returns(uint256) {
+  ) public view override returns(uint256) {
     uint256 balance = super.balanceOf(account);
-
-    if (balance < block.timestamp) {
-      return 0;
+    uint256 timestamp = timeNow();
+    unchecked {
+      return balance > timestamp ? balance - timestamp: 0;
     }
+  }
 
-    return balance - block.timestamp;
+  /**
+   * @dev Returns the number of decimals used to get its user representation.
+   * For example, if `decimals` equals `2`, a balance of `505` tokens should
+   * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+   */
+  function decimals() public pure override returns(uint8) {
+    return 3;
+  }
+
+  /**
+   * @dev Returns true if the account is expired
+   */
+  function isExpired(address account) public view returns(bool) {
+    return balanceOf(account) == 0 && super.balanceOf(account) > 0;
+  }
+
+  /**
+   * @dev Returns the end of the `account`
+   */
+  function endOf(address account) public view returns(uint256) {
+    return super.balanceOf(account);
+  }
+
+  /**
+   * @dev Returns the blocktime now in milliseconds
+   */
+  function timeNow() public view returns(uint256) {
+    return block.timestamp * _TO_MILLISEC;
   }
 
   /**
    * @dev Returns the amount of tokens in existence.
    */
-  function totalSupply() public view virtual override returns(uint256) {
-    uint256 epoch = block.timestamp * _recipients;
-    uint256 supply = super.totalSupply();
-
-    if (supply < epoch) {
-      return 0;
+  function totalSupply() public view override returns(uint256) {
+    uint256 supply = super.totalSupply() - _lastBurnedSupply;
+    uint256 active = timeNow() * _lastActiveAccounts;
+    unchecked {
+      return supply > active ? supply - active: 0;
     }
-
-    return supply - epoch;
-  }
-
-  /**
-   * @dev Returns the total recipients historically with time
-   */
-  function totalRecipients() public view returns(uint256) {
-    return _recipients;
   }
 
   // ============ Write Methods ============
@@ -74,15 +93,53 @@ contract InTime is Context, AccessControl, ERC20 {
   /**
    * @dev Creates `amount` new tokens for `to`.
    */
-  function mint(address to, uint256 amount) external onlyRole(_MINTER_ROLE) {
+  function mint(
+    address to, 
+    uint256 milliSeconds
+  ) external onlyRole(_MINTER_ROLE) {
+    //if they have no balance
     if (balanceOf(to) == 0) {
-      _recipients ++;
-      _mint(to, block.timestamp + amount);
-      return;
+      //require that they never had a balance (new account)
+      //you only get one life...
+      require(
+        super.balanceOf(to) == 0, 
+        "InTime: minting to expired account"
+      );
+      //if no more active accounts
+      if (totalSupply() == 0) {
+        _lastActiveAccounts = 0;
+        _lastBurnedSupply = super.totalSupply();
+      }
+      //now we can welcome a new account
+      _lastActiveAccounts ++;
+      //set like what time it will expire
+      milliSeconds += timeNow();
     }
 
-    _mint(to, amount);
+    //extended time!
+    _mint(to, milliSeconds);
   }
 
   // ============ Internal Methods ============
+
+  /**
+   * @dev Hook that is called before any transfer of tokens. This 
+   * includes minting and burning.
+   */
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal virtual override {
+    //if they are transferring
+    if (from != address(0) && to != address(0)) {
+      //require they have enough funds
+      require(
+        balanceOf(from) >= amount, 
+        "InTime: transfer amount exceeds balance"
+      );
+      //require they are not transferring to an expired account
+      require(!isExpired(to), "InTime: transfer to expired account");
+    }
+  }
 }
--- tests/InTime.test.js
+++ tests/InTime.test.js
@@ -4,7 +4,14 @@ const InTime = Artifact.get('InTime')
 
 contract('InTime', _ => {
   before(async function () {
-    await signers(this, [ 'admin', 'holder1', 'holder2' ])
+    await signers(this, [ 
+      'admin', 
+      'holder1', 
+      'holder2', 
+      'holder3', 
+      'holder4' , 
+      'holder5' 
+    ])
     const admin = this.admin.address
     this.token = await InTime.owner(admin).deploy(admin)
     await this.token.with(admin).grantRole(role('MINTER_ROLE'), admin)
@@ -12,18 +19,18 @@ contract('InTime', _ => {
 
   it('Should mint', async function () {
     const { token, admin, holder1, holder2 } = this
-    //mint holder1 100 tokens
-    await token.with(admin).mint(holder1.address, 100)
-    expect(await token.balanceOf(holder1.address)).to.equal(100)
-    expect(await token.totalSupply()).to.equal(100)
-    //mint holder1 another 100 tokens
-    await token.mint(holder1.address, 100)
-    expect(await token.balanceOf(holder1.address)).to.equal(199)
-    expect(await token.totalSupply()).to.equal(199)
-    //mint holder2 200 tokens
-    await token.mint(holder2.address, 200)
-    expect(await token.balanceOf(holder2.address)).to.equal(200)
-    expect(await token.totalSupply()).to.equal(398)
+    //mint holder1 100 seconds (we mint in milliseconds)
+    await token.with(admin).mint(holder1.address, 100000)
+    expect(await token.balanceOf(holder1.address)).to.equal(100000)
+    expect(await token.totalSupply()).to.equal(100000)
+    //mint holder1 another 100 seconds
+    await token.mint(holder1.address, 100000)
+    expect(await token.balanceOf(holder1.address)).to.equal(199000)
+    expect(await token.totalSupply()).to.equal(199000)
+    //mint holder2 200 seconds
+    await token.mint(holder2.address, 200000)
+    expect(await token.balanceOf(holder2.address)).to.equal(200000)
+    expect(await token.totalSupply()).to.equal(398000)
   })
 
   it('Should fastforward 7 sec later', async function() {
@@ -34,13 +41,13 @@ contract('InTime', _ => {
   it('Should decrease time', async function () {
     const { token, holder1, holder2 } = this
     
-    expect(await token.balanceOf(holder1.address)).to.equal(192)
-    expect(await token.balanceOf(holder2.address)).to.equal(194)
-    expect(await token.totalSupply()).to.equal(386)
+    expect(await token.balanceOf(holder1.address)).to.equal(192000)
+    expect(await token.balanceOf(holder2.address)).to.equal(194000)
+    expect(await token.totalSupply()).to.equal(386000)
   })
 
-  it('Should fastforward 200 sec later', async function() {
-    await ethers.provider.send('evm_increaseTime', [200]); 
+  it('Should fastforward 200 seconds later', async function() {
+    await ethers.provider.send('evm_increaseTime', [199]); 
     await ethers.provider.send('evm_mine');
   })
 
@@ -50,5 +57,81 @@ contract('InTime', _ => {
     expect(await token.balanceOf(holder1.address)).to.equal(0)
     expect(await token.balanceOf(holder2.address)).to.equal(0)
     expect(await token.totalSupply()).to.equal(0)
+
+    await expect(//you only have 1 life
+      token.mint(holder1.address, 100)
+    ).to.be.revertedWith('InTime: minting to expired account')
+
+    await expect(//you only have 1 life
+      token.mint(holder2.address, 100)
+    ).to.be.revertedWith('InTime: minting to expired account')
+  })
+
+  it('Should add time', async function () {
+    const { token, holder3 } = this
+    
+    //mint holder3 100 sec
+    await token.mint(holder3.address, 100000)
+    expect(await token.balanceOf(holder3.address)).to.equal(100000)
+    expect(await token.totalSupply()).to.equal(100000)
+  })
+
+  it('Should fastforward 50 seconds later', async function() {
+    await ethers.provider.send('evm_increaseTime', [50]); 
+    await ethers.provider.send('evm_mine');
+  })
+
+  it('Should add time', async function () {
+    const { token, holder3, holder4 } = this
+
+    expect(await token.balanceOf(holder3.address)).to.equal(50000)
+    expect(await token.totalSupply()).to.equal(50000)
+    
+    //mint holder4 150 sec
+    await token.mint(holder4.address, 150000)
+    expect(await token.balanceOf(holder4.address)).to.equal(150000)
+    expect(await token.totalSupply()).to.equal(199000)
+  })
+
+  it('Should fastforward 50 seconds later', async function() {
+    await ethers.provider.send('evm_increaseTime', [49]); 
+    await ethers.provider.send('evm_mine');
+  })
+
+  it('Should add time', async function () {
+    const { token, holder3, holder4, holder5 } = this
+
+    expect(await token.balanceOf(holder3.address)).to.equal(0)
+    expect(await token.balanceOf(holder4.address)).to.equal(101000)
+    expect(await token.totalSupply()).to.equal(101000)
+    
+    //mint holder5 100 sec
+    await token.mint(holder5.address, 100000)
+    expect(await token.balanceOf(holder5.address)).to.equal(100000)
+    expect(await token.totalSupply()).to.equal(199000)
+  })
+
+  it('Should not transfer', async function () {
+    const { token, holder3, holder5 } = this
+
+    await expect(//transferring to expired account
+      token.with(holder5).transfer(holder3.address, 20000)
+    ).to.be.revertedWith('InTime: transfer to expired account')
+
+    await expect(//no balance
+      token.with(holder5).transfer(holder3.address, 101000)
+    ).to.be.revertedWith('InTime: transfer amount exceeds balance')
+
+    await expect(//no balance
+      token.with(holder3).transfer(holder5.address, 10)
+    ).to.be.revertedWith('InTime: transfer amount exceeds balance')
+  })
+
+  it('Should transfer', async function () {
+    const { token, holder4, holder5 } = this
+
+    await token.with(holder5).transfer(holder4.address, 20000)
+    expect(await token.balanceOf(holder4.address)).to.equal(116000)
+    
   })
 })
